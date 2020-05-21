c45907c82c388a3163545f75bc58f8b5cb34c495
tzw <tzw@tdcare.cn>
2020-05-21 21:24:05 +0800

    修改上游项目连接池

--- eventuate-local-java-cdc-connector-unified/src/main/java/io/eventuate/local/unified/cdc/pipeline/common/factory/CommonCdcPipelineReaderFactory.java
+++ eventuate-local-java-cdc-connector-unified/src/main/java/io/eventuate/local/unified/cdc/pipeline/common/factory/CommonCdcPipelineReaderFactory.java
@@ -21,6 +21,8 @@ abstract public class CommonCdcPipelineReaderFactory<PROPERTIES extends CdcPipel
   protected MeterRegistry meterRegistry;
   protected LeaderSelectorFactory leaderSelectorFactory;
   protected BinlogEntryReaderProvider binlogEntryReaderProvider;
+  protected ConnectionPoolConfigurationProperties connectionPoolConfigurationProperties;
+
 
   @Autowired
   PoolConfigProperties poolConfigProperties;
@@ -32,7 +34,7 @@ abstract public class CommonCdcPipelineReaderFactory<PROPERTIES extends CdcPipel
     this.meterRegistry = meterRegistry;
     this.leaderSelectorFactory = leaderSelectorFactory;
     this.binlogEntryReaderProvider = binlogEntryReaderProvider;
-//    this.connectionPoolConfigurationProperties = connectionPoolConfigurationProperties;
+    this.connectionPoolConfigurationProperties = connectionPoolConfigurationProperties;
   }
 
   public abstract READER create(PROPERTIES cdcPipelineReaderProperties);
